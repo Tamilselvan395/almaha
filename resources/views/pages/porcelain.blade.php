02734df9ed00eeb9df0bddb45292dfcc9447f9cf
Tamilselvan395 <saitamil395@gmail.com>
2026-07-06 15:34:40 +0530

    schema error slove

--- resources/views/pages/porcelain.blade.php
+++ resources/views/pages/porcelain.blade.php
@@ -6,7 +6,7 @@
 @section('schema')
 <script type="application/ld+json">
 {
-  "@context": "https://schema.org",
+  "@@context": "https://schema.org",
   "@graph": [
     {
       "@type": "CollectionPage",
